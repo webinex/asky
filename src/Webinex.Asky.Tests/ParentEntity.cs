a955ada841c4b27684a270409772cf32bd334333
KyMback <Seregick@hotmail.com>
2023-08-18 13:03:13 +0300

    Fix: Fix guid nested collection in operator search

---
+++ src/Webinex.Asky.Tests/ParentEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Webinex.Asky.Tests;
+
+public class ParentEntity<T>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<NestedCollectionEntity<T>> Nested { get; set; }
+
+    protected ParentEntity()
+    {
+    }
+
+    public ParentEntity(string name, ICollection<NestedCollectionEntity<T>> nested)
+    {
+        Name = name;
+        Nested = nested;
+    }
+}
+
+public class NestedCollectionEntity<T>
+{
+    public Guid Id { get; set; }
+    public T Value { get; set; }
+
+    protected NestedCollectionEntity()
+    {
+    }
+
+    public NestedCollectionEntity(T value)
+    {
+        Value = value;
+    }
+}
